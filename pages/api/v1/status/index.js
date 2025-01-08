7ffa0451dfea4f05ea5d9925d810522cccb63d34
Guilherme Anjolette <124743404+Guilherme-S222@users.noreply.github.com>
2025-01-08 16:22:25 +0000

    Iniciando construção do endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "status" });
+  // Data de atualização
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
+
+  // Versão do PostgreSql
+  const dbVersion = await database.query("SELECT version()");
+  console.log(dbVersion.rows);
+
+  // Máximo de conexões
+  const maxConnections = await database.query("SHOW max_connections");
+  console.log(maxConnections.rows);
+
+  // Conexões ativas
+  const usedConnections = await database.query(
+    "SELECT COUNT(*) AS UsedConnections FROM pg_stat_activity",
+  );
+  console.log(usedConnections.rows);
 }
 
 export default status;
