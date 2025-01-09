eda0ecc936896795bb1af1ffc6f522d6e323639f
Guilherme Anjolette <124743404+Guilherme-S222@users.noreply.github.com>
2025-01-09 03:54:44 +0000

    Obtendo `server_version` e `max_connections` do PostreSQL

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,26 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  // Data de atualização
   const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
   response.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+      },
+    },
   });
-
-  // Versão do PostgreSql
-  const dbVersion = await database.query("SELECT version()");
-  console.log(dbVersion.rows);
-
-  // Máximo de conexões
-  const maxConnections = await database.query("SHOW max_connections");
-  console.log(maxConnections.rows);
-
-  // Conexões ativas
-  const usedConnections = await database.query(
-    "SELECT COUNT(*) AS UsedConnections FROM pg_stat_activity",
-  );
-  console.log(usedConnections.rows);
 }
 
 export default status;
