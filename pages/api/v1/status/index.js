a4737c3086ac3a74369c4820c278ac463c5a0ab1
Guilherme Anjolette <124743404+Guilherme-S222@users.noreply.github.com>
2025-01-30 16:02:48 +0000

    adding comments to the code

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,27 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
+  // Armazenando a data atual das informações
   const updatedAt = new Date().toISOString();
 
+  // Recuperando a versão do banco de dados PostgreSQL
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
+  //Recuperando o número máximo de conexões permitidas
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
-
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
 
+  // Recuperando a quantidade de conexões ativas no momento
   const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
-
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
 
+  // Enviando todas essas informações como resposta para o endpoint
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
